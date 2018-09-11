d2c90b47f34c7877abd5d0269bc8393ed5239bdd
SerafimPoch <serpochapinsky@gmail.com>
2018-09-11 12:46:04 +0300

    add test for actions

---
+++ src/tests/actions.test.js
@@ -0,0 +1,31 @@
+import * as types from "../store/actions/actionTypes";
+import * as action from "../store/actions/";
+
+describe("actions", () => {
+  it("should create add action", () => {
+    const task = "Fishing";
+    const expectedAction = {
+      type: types.ADD_TASK,
+      payload: task
+    };
+    expect(action.addTask(task)).toEqual(expectedAction);
+  });
+
+  it("should create remove action", () => {
+    const task = "Fishing";
+    const expectedAction = {
+      type: types.REMOVE_TASK,
+      payload: task
+    };
+    expect(action.remove(task)).toEqual(expectedAction);
+  });
+
+  it("should create search action", () => {
+    const task = "Fishing";
+    const expectedAction = {
+      type: types.SEARCH_TASK,
+      payload: task
+    };
+    expect(action.search(task)).toEqual(expectedAction);
+  });
+});
